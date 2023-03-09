6f03b57c5ba589851fb87a24f1b784ae31c6c532
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2023-03-09 13:49:44 +0100

    add comment

--- lib/blog_web/schema.ex
+++ lib/blog_web/schema.ex
@@ -61,4 +61,20 @@ defmodule BlogWeb.Schema do
       end)
     end
   end
+
+  input_object :add_comment_attrs do
+    field :post_id, non_null(:integer)
+    field :author, non_null(:string)
+    field :content, non_null(:string)
+  end
+
+  mutation do
+    field :add_comment, :comment do
+      arg(:attrs, non_null(:add_comment_attrs))
+
+      resolve(fn %{attrs: attrs}, _ ->
+        Blog.Comments.create_comment(attrs)
+      end)
+    end
+  end
 end
